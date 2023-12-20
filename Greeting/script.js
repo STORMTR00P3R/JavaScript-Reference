1f4823ae006b19067290d51d3431b5530dff6c2d
STORMTR00P3R <noorblum16@gmail.com>
2023-12-19 17:51:31 -0800

    e

--- Greeting/script.js
+++
@@ -1,7 +0,0 @@
-function createGreeting(name) {
-    const greetingHeader = document.createElement('h1');
-    greetingHeader.innerText = "Greetings, " + name;
-    document.querySelector('body').appendChild(greetingHeader);
-}
-
-createGreeting(prompt("Enter your name: "));
