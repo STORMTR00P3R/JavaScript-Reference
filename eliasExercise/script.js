d93c0dab838a391b5ae9f24bf5b8602830f2cdfc
STORMTR00P3R <noorblum16@gmail.com>
2024-01-13 15:59:53 -0800

    e

--- eliasExercise/script.js
+++ eliasExercise/script.js
@@ -3,3 +3,11 @@ headerElement.textContent = "A good title";
 
 const paragraphElement = document.getElementById('paragraphElement');
 paragraphElement.textContent = "JavaScript (/ˈdʒɑːvəskrɪpt/), often abbreviated as JS, is a programming language and core technology of the World Wide Web, alongside HTML and CSS. As of 2023, 98.7% of websites use JavaScript on the client side for webpage behavior,[10] often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices.";
+
+const myDiv = document.createElement('div');
+myDiv.style.border = '4px solid red';
+myDiv.style.width = '100px';
+myDiv.style.height = '100px';
+myDiv.style.backgroundColor = 'green';
+myDiv.style.borderRadius = '15px'
+document.body.appendChild(myDiv);
