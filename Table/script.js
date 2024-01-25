865e64c3cbaaf525847054f1a46ecf6cc85adf59
STORMTR00P3R <noorblum16@gmail.com>
2024-01-24 17:14:50 -0800

    please

--- Table/script.js
+++ Table/script.js
@@ -5,32 +5,34 @@ async function fetchJSON() {
         console.log(jsonData);
         return jsonData;
     } catch(error) {
-        console.error("Error fetch JSON: ", error);
+        console.error("Error fetching JSON: ", error);
     }
-} 
+}
 
 function populateTable(data) {
     const tableBody = document.getElementById("games-table").getElementsByTagName("tbody")[0];
     console.log(tableBody);
 
     data.games.forEach(game => {
-        const row = tableBody.insertRow();
-        const cellID = row.insertCell(0);
-        const cellName = row.insertCell(1);
-        const cellYear = row.insertCell(2);
+        const row           = tableBody.insertRow();
+        const cellID        = row.insertCell(0);
+        const cellName      = row.insertCell(1);
+        const cellYear      = row.insertCell(2);
         const cellPublisher = row.insertCell(3);
-        const cellImage = row.insertCell(4);
+        const cellImage     = row.insertCell(4);
 
-        const gameImage = document.createElement("img");
-        gameImage.src= game.imageURL;
-        gameImage.style.height = '50px';
-        gameImage.style.width = '50px';
-        gameImage.style.borderRadius = '50px';
+        const gameImage              = document.createElement("img");
+        gameImage.src                = game.imageURL;
+        gameImage.style.height       = '50px';
+        gameImage.style.width        = '50px';
+        gameImage.style.borderRadius = '10px';
         cellImage.appendChild(gameImage);
 
-        cellID.textContent = game.id;
-        cellName.textContent = game.name;
-        cellYear.textContent = game.year;
+        cellID.textContent        = game.id;
+        cellName.textContent      = game.name;
+        cellYear.textContent      = game.year;
         cellPublisher.textContent = game.publisher;
-    })
+    });
 }
+
+fetchJSON().then(populateTable);
